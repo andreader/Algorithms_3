f46cadbaa1037c7b195c5a861fd0c6fc4289d4ec
Андрей <79538233369@ya.ru>
2024-06-29 17:48:20 +0500

    initial

---
+++ src/main/java/pro/sky/algorithms_3/Algorithms3Application.java
@@ -0,0 +1,13 @@
+package pro.sky.algorithms_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Algorithms3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Algorithms3Application.class, args);
+    }
+
+}
